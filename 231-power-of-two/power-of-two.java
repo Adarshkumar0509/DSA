1970b8bbc7a5c9c8e41a272a0ab90b2d29ad1ce4
Adarsh Kumar <162141376+Adarshkumar0509@users.noreply.github.com>
2026-02-03 22:20:36 +0530

    Update power-of-two.java

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,4 +1,5 @@
-//power of TWO    ... Now in the same way you can solve power of 3,4,5,etc.
+//power of TWO    ... Now in the same way you can solve power of 3,4,5,5 etc steps will be same
+
 class Solution {
     public boolean isPowerOfTwo(int n) {
         if(n<1){
